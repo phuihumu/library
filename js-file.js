9993dcb49612fe8e2358ecd5ce001bb227ec5000
phuihumu <priscilla.hui@sjsu.edu>
2021-11-19 21:00:32 -0800

    Added a function that displays all the books in the library in a table. Added the table and table header elements in the HTML file.

--- js-file.js
+++ js-file.js
@@ -1,3 +1,6 @@
+const container = document.querySelector('.container');
+const table = document.querySelector('table');
+
 let myLibrary = [];
 
 //Constructor for making "Book" objects
@@ -14,3 +17,33 @@ Book.prototype.info = function() {
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
+
+function displayAllBooks() {
+    for (const book of myLibrary) {
+        let bookEntry = document.createElement('tr');
+        let bookEntryTitle = document.createElement('td');
+        let bookEntryAuthor = document.createElement('td');
+        let bookEntryPages = document.createElement('td');
+        let bookEntryRead = document.createElement('td');
+        bookEntryTitle.innerHTML = book.title;
+        bookEntryAuthor.innerHTML = book.author
+        bookEntryPages.innerHTML = book.pages;
+        bookEntryRead.innerHTML = book.read;
+        table.appendChild(bookEntry);
+        table.appendChild(bookEntryTitle);
+        table.appendChild(bookEntryAuthor);
+        table.appendChild(bookEntryPages);
+        table.appendChild(bookEntryRead);
+    }
+
+}
+
+harryPotter = new Book("Harry Potter", "J.K. Rowling", 500, "read");
+gatsby = new Book("The Great Gatsby", "F. Scott Fitzgeral", 400, "read");
+console.log(harryPotter.info());
+
+addBookToLibrary(harryPotter);
+addBookToLibrary(gatsby);
+console.log(myLibrary);
+
+displayAllBooks();
