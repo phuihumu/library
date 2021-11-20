fedf8b5372b1930a03c7c8c3d5022e597df6341a
phuihumu <priscilla.hui@sjsu.edu>
2021-11-19 18:24:21 -0800

    Created a constructor for Book objects that includes properties and a function to return those properties.

---
+++ js-file.js
@@ -0,0 +1,8 @@
+//Constructor for making "Book" objects
+function Book(title, author, pages, read)
+{
+    this.title = tile;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
