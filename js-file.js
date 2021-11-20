c3c8330bfe40df09e3e7e5a3e043e533b8a322de
phuihumu <priscilla.hui@sjsu.edu>
2021-11-19 23:47:39 -0800

    Changes made to the form in HTML and CSS. The add book form now adds a new book to library with information inputted by the user, and is displayed when the Add Book button is clicked. After the Add Book button is clicked and the display is updated, the form is hidden once again.

--- js-file.js
+++ js-file.js
@@ -1,5 +1,11 @@
 const container = document.querySelector('.container');
 const table = document.querySelector('table');
+const tableBody = document.querySelector('tbody');
+const bookForm = document.querySelector('.bookForm');
+const newBookTitle = document.querySelector('#title');
+const newBookAuthor = document.querySelector('#author');
+const newBookPages = document.querySelector('#pages');
+const newBookRead = document.querySelector('#read');
 
 let myLibrary = [];
 
@@ -19,6 +25,8 @@ function addBookToLibrary(book) {
 }
 
 function displayAllBooks() {
+    while (tableBody.firstChild) tableBody.removeChild(tableBody.firstChild);
+
     for (const book of myLibrary) {
         let bookEntry = document.createElement('tr');
         let bookEntryTitle = document.createElement('td');
@@ -29,26 +37,33 @@ function displayAllBooks() {
         bookEntryAuthor.innerHTML = book.author
         bookEntryPages.innerHTML = book.pages;
         bookEntryRead.innerHTML = book.read;
-        table.appendChild(bookEntry);
-        table.appendChild(bookEntryTitle);
-        table.appendChild(bookEntryAuthor);
-        table.appendChild(bookEntryPages);
-        table.appendChild(bookEntryRead);
+        bookEntry.appendChild(bookEntryTitle);
+        bookEntry.appendChild(bookEntryAuthor);
+        bookEntry.appendChild(bookEntryPages);
+        bookEntry.appendChild(bookEntryRead);
+        tableBody.appendChild(bookEntry);
     }
 }
 
 const newBookBtn = document.querySelector("#new");
 newBookBtn.addEventListener("click", () => {
-    document.querySelector(".bookForm").style.display = "flex";
+    bookForm.style.display = "flex";
 });
+
+const addBookButton = document.querySelector("#add");
+addBookButton.addEventListener("click", () => {
+        addBookToLibrary(new Book(newBookTitle.value, newBookAuthor.value, newBookPages.value, newBookRead.value));
+        bookForm.style.display = "none";
+        displayAllBooks();
+    });
 
 
 harryPotter = new Book("Harry Potter", "J.K. Rowling", 500, "read");
 gatsby = new Book("The Great Gatsby", "F. Scott Fitzgeral", 400, "read");
-console.log(harryPotter.info());
+//console.log(harryPotter.info());
 
 addBookToLibrary(harryPotter);
 addBookToLibrary(gatsby);
 console.log(myLibrary);
 
-displayAllBooks();
+//displayAllBooks();
