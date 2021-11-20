c82736afa7652bd2aa7602eb2c38a9ba345df82c
phuihumu <priscilla.hui@sjsu.edu>
2021-11-19 18:58:19 -0800

    Made changes to the function, info, so it returns the properties of the Book that calls it.

--- js-file.js
+++ js-file.js
@@ -1,8 +1,11 @@
 //Constructor for making "Book" objects
 function Book(title, author, pages, read)
 {
-    this.title = tile;
+    this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.info = function() {
+        return (this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read);
+    }
 }
